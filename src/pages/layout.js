873855369b4a22de39806a6f5d6637579a37220c
Nij Mehar <nij.mehar@expinfi.com>
2023-09-13 17:04:43 +0530

    added Inter

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,3 +1,4 @@
+import { Inter } from 'next/font/google'
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Inter.className}>{children}</body>
     </html>
   )
 }
