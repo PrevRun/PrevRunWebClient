43f0196609b0970f9cf673b6d73e2463f27cbfd3
07anhad <anhadparashar07@gmail.com>
2023-09-13 16:04:55 +0530

    prevrun.com coming soon -> main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from 'sections/banner';
-import Support from 'sections/support';
-import PremiumFeature from 'sections/premium-feature';
-import AppFeature from 'sections/app-feature';
-import Dashboard from 'sections/dashboard';
-import Pricing from 'sections/pricing';
-import Testimonials from 'sections/testimonials';
-import Subscribe from 'sections/subscribe';
+import styles from './layout.module.css'
 
-export default function IndexPage() {
+export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <SEO
-          title="Startup Agency Alpha Landing"
-          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
-        />
-        <Banner />
-        <Support />
-        <PremiumFeature />
-        <AppFeature />
-        <Dashboard />
-        <Pricing />
-        <Testimonials />
-        <Subscribe />
-      </Layout>
-    </ThemeProvider>
-  );
+    <main className={styles.main}>
+      <style>
+        {`
+          body {
+            background-color: black;
+            overflow-y: hidden;
+          }
+
+          .hotPinkText {
+            background: linear-gradient(45deg, #8A2BE2, #0BFFFF);
+            background-clip: text;
+            -webkit-background-clip: text;
+            color: transparent;
+            text-fill-color: transparent;
+          }
+        `}
+      </style>
+
+      <div className={styles.description}>
+        <div>
+         
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <h1 className={`${styles.comingSoon} hotPinkText`} style={{ fontSize: '4em', marginBottom:'8rem' }}>
+          Coming Soon...
+        </h1>
+      </div>
+    </main>
+  )
 }
