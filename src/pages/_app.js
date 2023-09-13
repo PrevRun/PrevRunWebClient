d807acfafd933bdbc9aa7ce728b6f694ae994f89
07anhad <anhadparashar07@gmail.com>
2023-09-13 16:39:33 +0530

    globals.css in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import Router from 'next/router';
+import '../pages/globals.css'
 // import { initGA, logPageView } from 'analytics';
 import 'rc-drawer/assets/index.css';
 import 'rc-tabs/assets/index.css';
