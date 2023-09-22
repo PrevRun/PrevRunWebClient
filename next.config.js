58b9e49c33aaf34a4287f320a689e8501b3f9b0b
07anhad <anhadparashar07@gmail.com>
2023-09-22 23:47:17 +0530

    nextjs.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = {
-  target: 'serverless',
-  async rewrites() {
-    return [
-      {
-        source: '/privacy.html',
-        destination: '/privacy',
-      },
-      {
-        source: '/terms.html',
-        destination: '/terms',
-      },
-    ]
-  },
-}
-
+const nextConfiguration = {
+  target: 'serverless', 
+};
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
