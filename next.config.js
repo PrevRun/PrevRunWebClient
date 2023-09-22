889df8215d83d1939d10b04542ab390e4c556cf7
07anhad <anhadparashar07@gmail.com>
2023-09-22 23:39:11 +0530

    next-config.js modify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-const nextConfiguration = {
-  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
-};
+module.exports = {
+  target: 'serverless',
+  async rewrites() {
+    return [
+      {
+        source: '/privacy.html',
+        destination: '/privacy',
+      },
+      {
+        source: '/terms.html',
+        destination: '/terms',
+      },
+    ]
+  },
+}
+
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
